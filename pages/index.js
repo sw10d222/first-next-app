916f0fa234614bf31bf0a4ce3e50700445e106c6
sw10d222 <sw10d222@gmail.com>
2021-03-04 16:14:35 +0800

    Fix casing discrepancies all.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Player from "../components/Player";
-import PlayerCSS from "../components/PlayerCss";
+import Player from "../components/Player.js";
+import PlayerCSS from "../components/PlayerCss.js";
 
 export default function Home() {
   const videoJsOptions = {
